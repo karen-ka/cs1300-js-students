e13764af362af51e83c6cc0d7bd2a9c8a8b9f956
Karen Ka <karen925@gmail.com>
2020-10-29 20:24:41 -0400

    basic site

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+// var apiToken = "?token=eto-vXCwQu7OMYwUI1VVwOCd_z1TXJi7ZIG1cxS7y5E";
+var apiToken = "?token=KkDoOS8-aGst16DxpXSYbvdu39BFrvz-ferm-CjPMMc";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +26,41 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      const plantData = JSON.parse(request.response).data;
+      console.log(plantData);
+      let ids = plantData.filter((x) => {
+        return x['id'] % 2 === 0;
+      });
+      // console.log(ids);
+      addPlants(ids);
     })
 );
 
+// STUDENT
+function addPlants(data) {
+  // let li = [];
+  // let s = "";
+
+  // ids.foreach((x) => {
+  //   document.getElementById("addafterme").insertAdjacentHTML("afterend", 
+  //   `<div class="holder"><img src=${data[i]['image_url']} alt='Plant'></div>`);
+  // })
+
+  for(let i = 0; i < data.length; i++){ 
+    // li.append(data[i][1]);
+    // li.push(data[i]['common_name'])
+    // s += data[i]['common_name'] + "\n";
+    document.getElementById("content").insertAdjacentHTML("afterend", 
+    `<div class="holder"><figure><img src=${data[i]['image_url']} alt='Plant'><figcaption>${data[i]['common_name']}</figcaption></figure></div>`);
+  }
+  // document.getElementById("para").innerHTML += s;
+  console.log('got here')
+}
+
+
+
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+// function fun() {
+//   document.getElementById()
+// }
